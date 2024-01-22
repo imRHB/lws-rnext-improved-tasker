68ae29e8f00576adcaebbbac21a2369cc0904dc9
Rakib Hasan Babu <rhbabu3@gmail.com>
2024-01-22 20:34:23 +0600

    improved tasker initialized and configured

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main className="p-8">
+            <h3 className="text-5xl font-extrabold text-violet-500 text-center underline underline-offset-2">
+                Improved Tasker
+            </h3>
+        </main>
+    );
+}
+
+export default App;
